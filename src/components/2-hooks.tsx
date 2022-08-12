7dd37ed7a1bf0198bbab33d046fac7c99b651343
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-12 10:34:56 +0100

    finished working on the adding of items to the todo

--- src/components/2-hooks.tsx
+++ src/components/2-hooks.tsx
@@ -1,9 +1,34 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 /**
 remove a todoItem from the list of todo
 create a separate component for adding an item to the todo
 */
 
+
+// the component that will be used to add a new item to the todoList
+type addToListProps = {
+    handleClick: (description: todoState['description']) => void;
+}
+
+const AddItemToTheTodoList = ({handleClick}: addToListProps) => {
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    return (
+        <div className="">
+            <input ref={inputRef} type="text" placeholder='Add an item to your todo list'/>
+            <button onClick={() => {
+                if (inputRef.current && inputRef.current.value.length > 0) {
+                    handleClick(inputRef.current.value)
+                    inputRef.current.value = ''
+                }
+            }}>Add item</button>
+        </div>
+    )
+}
+
+
+
+// creates the initial todo items
 type todoState = {
     id: number,
     description: string
@@ -14,17 +39,13 @@ const itemsList: todoState[] = [
     {id:2, description:'Will see my love soon'},
 ]
 
-const AddItemToTheTodoList = () => {
-    return (
-        <div className="">
-            <input type="text" placeholder='Add an item to your todo list'/>
-            <button>Add item</button>
-        </div>
-    )
-}
-
 export default function TodoComp() {
     const [items, setItems] = useState<todoState[]>(itemsList)
+
+    const addItem = (description: todoState['description']) => {
+        setItems([...items, {id:items[items.length - 1].id + 1 || items.length+1, description}])
+        // setItems([...items, {id:items.length+1, description}])
+    }
 
     const removeItem = (idToDelete: todoState['id']) => {
         const newList = items.filter(echItem => echItem.id !== idToDelete)
@@ -32,22 +53,21 @@ export default function TodoComp() {
     }
 
     return (
-        <div>
-            {items.map((item) => {
-                return (
-                    <div className="mainCover">
+        <div className="mainCover">
+            <AddItemToTheTodoList handleClick={addItem} />
 
-                        <div className="todoCover">
-                            <div className="todoItem" key={item.id}>
-                                <div className="">{item.description}</div>
-                                <div className="delete_am">
-                                    <button onClick={() => removeItem(item.id)}>X</button>
-                                </div>
+            <div className="todoCover">
+                {items.map((item) => {
+                    return (
+                        <div className="todoItem" data-hold={item.id} key={item.id}>
+                            <div className="">{item.description}</div>
+                            <div className="delete_am">
+                                <button onClick={() => removeItem(item.id)}>X</button>
                             </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     )
 }

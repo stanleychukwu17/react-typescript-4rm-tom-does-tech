340ad57b41ed693b6dfcd7b9f15a6deb9338b819
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-18 23:03:29 +0100

    worked on generics file

--- src/components/1-generics.tsx
+++ src/components/1-generics.tsx
@@ -11,7 +11,7 @@ export default function Box({children}: boxProps) {
 }
 
 /**
-1. So what if we wanted to passing in inline styling to the Box component
+1. So what if we wanted to pass inline styling to the Box component
 explanation: so when destructuring the props of Box1, we destructured 'children' separately and then destructured the remaining items into an object called rest
 */
 export function Box1({children, ...rest}: boxProps & React.CSSProperties) {
@@ -21,7 +21,7 @@ export function Box1({children, ...rest}: boxProps & React.CSSProperties) {
 }
 
 /**
-1. A button component that all buttons can take in attributes and properties of a button element
+1. A component that can take in attributes and properties of a button element
 with the addition of 'React.ButtonHTMLAttributes<HTMLButtonElement>', we can now add all the attributes of button element like (onClick, onBlur, onDoubleClick e.t.c)
 */
 type buttonProps = {
@@ -30,6 +30,18 @@ type buttonProps = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button({children, styles, ...rest}: buttonProps) {
+    return (
+        <button {...rest} style={styles}> {children} </button>
+    )
+}
+
+// or my own simple version
+type buttonProps_2 = {
+    children: React.ReactNode
+    styles?: React.CSSProperties
+} & React.ComponentProps<React.ElementType>
+
+export function Button2({children, styles, ...rest}: buttonProps_2) {
     return (
         <button {...rest} style={styles}> {children} </button>
     )
